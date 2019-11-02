c65db955ff2f073a209e1c26ef229e809ad26244
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-11-02 16:04:58 -0300

    js

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,11 @@
+var temp = parsefloat (document.getElementById(temperature).value);
+var wSpeed = parsefloat (document.getElementById(wind).value);
+
+wChill = 35.74 + 0.6215 * temp - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
+
+if (temp <= 50 && wSpeed >= 3) {
+    document.getElementById("windChill").innerHTML = wChill.toFixed(0) + "\xB0F";
+}
+else {
+    document.getElementById("windChill").innerHTML = "N/A"
+}
