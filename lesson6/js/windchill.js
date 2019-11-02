16ed52348447b38a7b98d5b1eadc54382b634cec
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-11-02 16:23:07 -0300

    parse

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,5 +1,5 @@
-var temp = parseFloat (document.getElementById(temperature).value);
-var wSpeed = parseFloat (document.getElementById(wind).value);
+var temp = parseFloat (document.getElementById(temperature).textContent);
+var wSpeed = parseFloat (document.getElementById(wind).textContent);
 
 wChill = 35.74 + 0.6215 * temp - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
 
