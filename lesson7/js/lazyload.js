b60277e1e14c4651ff068bb04431a1265d0011be
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-11-09 12:05:58 -0300

    commti

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -2,7 +2,7 @@ const images = document.querySelectorAll('img[data-src]');
 
 const imageOptions = {
     threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
+    rootMargin: "0px 0px 200px 0px"
 };
 
 const loadImages = (image) => {
