6007fefd0766e17038f0b1850c773522b141ce93
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-11-23 17:03:56 -0300

    api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,6 @@
 const weatherObject = new XMLHttpRequest();
 
-weatherObject.open("GET","https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=02ad1bdd62f910cc3245193223e89746ad&units=imperial", true);
+weatherObject.open("GET", "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=035595da61bbb78c1498b8e8863b9151&units=imperial", true);
 weatherObject.send();
 weatherObject.onload = function() {
    let weatherInfo = JSON.parse(weatherObject.responseText);
@@ -13,10 +13,3 @@ weatherObject.onload = function() {
 
 
 
-/*const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=20166b3a796e025a536c9db9d87c1ebd";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-  });*/
