18fec6d4bb996d00d869ff96aa5a793209388698
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-11-23 11:21:03 -0300

    id

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,6 @@
 const weatherObject = new XMLHttpRequest();
 
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=035595da61bbb78c1498b8e8863b9151&units=imperial", true);
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=20166b3a796e025a536c9db9d87c1ebd&units=imperial", true);
 weatherObject.send();
 weatherObject.onload = function() {
    let weatherInfo = JSON.parse(weatherObject.responseText);
