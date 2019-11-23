14d878488288979f88a578d3a17af1a6c3f2f69a
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-11-23 20:54:33 -0300

    api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,6 @@
 const weatherObject = new XMLHttpRequest();
 
-weatherObject.open("GET", "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=035595da61bbb78c1498b8e8863b9151&units=imperial", true);
+weatherObject.open("GET", "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=73bb36bc52c2326776438d6ada69eb9d&units=imperial", true);
 weatherObject.send();
 weatherObject.onload = function() {
    let weatherInfo = JSON.parse(weatherObject.responseText);
@@ -9,6 +9,11 @@ weatherObject.onload = function() {
    document.getElementById("place").innerHTML = weatherInfo.name;
    document.getElementById("currentTemp").innerHTML = weatherInfo.main.temp;
    document.getElementById("windSpeed").innerHTML = weatherInfo.wind.speed;
+
+   const iconcode = weatherInfo.weather[0].icon;
+   const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+   console.log(icon_path);
+   document.getElementById("weather_icon").src = icon_path;
 
 } // end of onload
 
