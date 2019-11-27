a3a358df2dc15a579f6f6fda53de6a8e7dfc5c7e
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-11-26 22:27:34 -0300

    js

--- lesson10/js/weatherInfo.js
+++ lesson10/js/weatherInfo.js
@@ -27,19 +27,40 @@ weatherObject.onload = function() {
 
 } // end of onload*/
 
-const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET", "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=73bb36bc52c2326776438d6ada69eb9d&units=imperial", true);
+const weatherObject = new XMLHttpRequest ();
+weatherObject.open("GET", "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=c4f26b47558cfc6a6aebf07554d017ad&units=imperial", true);
 weatherObject.send();
-weatherObject.onload = function() {
-   let weatherInfo = JSON.parse(weatherObject.responseText);
-   console.log(weatherInfo);
+weatherObject.onload = function () {
 
-   document.getElementById("currently").innerHTML = weatherInfo.weather[0].main;
-   document.getElementById("tempnow").innerHTML = weatherInfo.main.temp;
-   document.getElementById("humidity").innerHTML = weatherInfo.main.humidity;
-   document.getElementById("wind").innerHTML = weatherInfo.wind.speed;
+	let weatherInfo = JSON.parse (weatherObject.responseText);
+	console.log(weatherInfo);
+	document.getElementById("tempnow").textContent = weatherInfo.main.temp;
+	document.getElementById("humidity").textContent = weatherInfo.main.humidity;
+	document.getElementById("wind").textContent = weatherInfo.wind.speed;
+	var descriptionp = weatherInfo.weather['0'].description;
+
+	document.getElementById("currently").textContent = descriptionp;
+	console.log(descriptionp);
+
+	function doinputoutput() {
+    var a = parseFloat(document.getElementById('tempnow').textContent);
+    var b = parseFloat(document.getElementById('wind').textContent);
+  
+    result = (35.74 + (0.6215 * a) - (35.75 * Math.pow(b, 0.16)) + (0.4275 * a * Math.pow(b, 0.16)));
+  
+    if (result <= 50 && b > 3) {
+      document.getElementById('windChill').textContent = result.toFixed(0);
+    } else {
+      document.getElementById('windChill').textContent = "NA";
+    }
+  }
+  doinputoutput();
+
+
 
 }
+
+
 
 
 
