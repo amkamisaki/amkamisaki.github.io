e4ef7e571f62bbe76cc684ab260c3bb4189d420e
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-11-26 21:48:32 -0300

    js

--- lesson10/js/weatherInfo.js
+++ lesson10/js/weatherInfo.js
@@ -11,8 +11,8 @@ weatherObject.onload = function() {
    document.getElementById("wind").innerHTML = weatherInfo.wind.speed;
 
    function windChill() {
-    var temp = parseFloat(document.getElementById("tempnow").textContent);
-    var wSpeed = parseFloat(document.getElementById("wind").textContent);
+    var temp = parseFloat(document.getElementById("tempnow").innerHTML);
+    var wSpeed = parseFloat(document.getElementById("wind").innerHTML);
     
     wChill = 35.74 + 0.6215 * temp - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
     
