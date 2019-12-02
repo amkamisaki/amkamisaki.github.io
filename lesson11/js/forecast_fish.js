efec295d7823fd62560c4364062b1800cc869bbb
Armando Kamisaki Account <amkamisaki@hotmail.com>
2019-12-02 17:08:28 -0300

    fish

---
+++ lesson11/js/forecast_fish.js
@@ -0,0 +1,43 @@
+fetch('https://api.openweathermap.org/data/2.5/forecast?zip=83287&units=imperial&appid=73bb36bc52c2326776438d6ada69eb9d')
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+         let forecast = [];
+          let icon = [];
+          let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+         let list = jsObject.list;
+        let day = 1;
+        let ico = 1;
+        let d = 1;
+        var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+      for (let i = 1; i <= 5; i++) {
+        jsObject.list.forEach(x => {
+            if (x.dt_txt.includes('18:00:00')) {
+                forecast[day] = x.main.temp;
+                day++;
+                icon[ico] = x.weather[0].icon;
+                ico++;
+
+                 days[d] = new Date(x.dt_txt);
+                  n = weekday[days[i].getDay ()];
+                   d++;
+            
+            
+             let imagesrc = '//openweathermap.org/img/w/' +  icon[i] + '.png';
+             document.getElementById("icon" + i).setAttribute('src', imagesrc);
+             document.getElementById('temp' + i ).innerHTML = forecast[i];
+             document.getElementById('day' + i ).innerHTML =  n ;
+        }});
+
+  
+
+    }});
---
+++ lesson11/js/weatherInfo_fish.js
@@ -0,0 +1,39 @@
+const weatherObject = new XMLHttpRequest();
+weatherObject.open("GET", "https://api.openweathermap.org/data/2.5/weather?zip=83287&appid=035595da61bbb78c1498b8e8863b9151&units=imperial", true);
+weatherObject.send();
+weatherObject.onload = function() {
+   let weatherInfo = JSON.parse(weatherObject.responseText);
+   console.log(weatherInfo);
+
+   document.getElementById("currently").innerHTML = weatherInfo.weather[0].main;
+   document.getElementById("tempnow").innerHTML = weatherInfo.main.temp;
+   document.getElementById("humidity").innerHTML = weatherInfo.main.humidity;
+   document.getElementById("wind").innerHTML = weatherInfo.wind.speed;
+
+   function windChill() {
+    var temp = parseFloat(document.getElementById("tempnow").innerHTML);
+    var wSpeed = parseFloat(document.getElementById("wind").innerHTML);
+    
+    wChill = 35.74 + 0.6215 * temp - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
+    
+    if (temp <= 50 && wSpeed > 3) {
+        document.getElementById("windChill").innerHTML = wChill.toFixed(0) + "\xB0F";
+    }
+    else {
+        document.getElementById("windChill").innerHTML = "N/A";
+    }
+   }
+   windChill();
+
+} // end of onload
+
+
+
+
+
+
+
+
+    
+   
+
